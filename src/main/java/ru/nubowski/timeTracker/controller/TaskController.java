70de5247725c17ce737e58c9affcf0a5e81071c0
nubowski <nubowski@nnteam.ru>
2023-06-10 23:42:38 +0300

    feat: add LOGGER to TaskController + refactor

--- src/main/java/ru/nubowski/timeTracker/controller/TaskController.java
+++ src/main/java/ru/nubowski/timeTracker/controller/TaskController.java
@@ -1,5 +1,7 @@
 package ru.nubowski.timeTracker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
@@ -19,28 +22,42 @@ public class TaskController {
 
     @GetMapping
     public ResponseEntity<List<Task>> getAllTasks() {
-        return ResponseEntity.ok(taskService.getAllTasks());
+        LOGGER.info("Received request to get all tasks");
+        List<Task> tasks = taskService.getAllTasks();
+        LOGGER.info("Responding with {} tasks", tasks.size());
+        return ResponseEntity.ok(tasks);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.getTask(id));
+        LOGGER.info("Received request to get task with id: {}", id);
+        Task task = taskService.getTask(id);
+        LOGGER.info("Responding with task: {}", task);
+        return ResponseEntity.ok(task);
     }
 
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        return new ResponseEntity<>(taskService.saveTask(task), HttpStatus.CREATED);
+        LOGGER.info("Received request to create task: {}", task);
+        Task createdTask = taskService.saveTask(task);
+        LOGGER.info("Created task with id: {}", createdTask.getId());
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        LOGGER.info("Received request to update task with id: {} with data: {}", id, task);
         task.setId(id);
-        return ResponseEntity.ok(taskService.saveTask(task));
+        Task updatedTask = taskService.saveTask(task);
+        LOGGER.info("Updated task with id: {}", updatedTask.getId());
+        return ResponseEntity.ok(updatedTask);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        LOGGER.info("Received request to delete task with id: {}", id);
         taskService.deleteTask(id);
+        LOGGER.info("Deleted task with id: {}", id);
         return ResponseEntity.noContent().build();
     }
 }
