bbeaed4591b7b2be06af34889d5029e6d31bf768
nubowski <nubowski@nnteam.ru>
2023-06-11 00:29:56 +0300

    feat: add LOGGER to TimeLogController + refactor

--- src/main/java/ru/nubowski/timeTracker/controller/TimeLogController.java
+++ src/main/java/ru/nubowski/timeTracker/controller/TimeLogController.java
@@ -1,5 +1,7 @@
 package ru.nubowski.timeTracker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/time_logs")
 public class TimeLogController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimeLogController.class);
     private final TimeLogService timeLogService;
     private final TaskService taskService;
     private final UserService userService;
@@ -32,36 +35,56 @@ public class TimeLogController {
 
     @PostMapping("/start/{taskId}")
     public ResponseEntity<TimeLog> startTask(@PathVariable Long taskId) {
-        Task task = taskService.getTask(taskId);
-        TimeLog timeLog = timeLogService.startTask(task);
-        return new ResponseEntity<>(timeLog, HttpStatus.CREATED);
+        LOGGER.info("Received request to start task with id {}", taskId);
+        try {
+            Task task = taskService.getTask(taskId);
+            TimeLog timeLog = timeLogService.startTask(task);
+            LOGGER.info("Task with id {} has been started", taskId);  // dont think its necessary coz of custom ex, but just in case
+            return new ResponseEntity<>(timeLog, HttpStatus.CREATED);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while starting task with id {}", taskId, e);
+            throw e;
+        }
     }
 
     @PostMapping("/stop/{taskId}")
     public ResponseEntity<TimeLog> stopTask(@PathVariable Long taskId) {
+        LOGGER.info("Received request to stop task with id {}", taskId);
         Task task = taskService.getTask(taskId);
         TimeLog timeLog = timeLogService.stopTask(task);
+        LOGGER.info("Task with id {} has been stopped", taskId);
         return ResponseEntity.ok(timeLog);
     }
 
     @GetMapping
     public ResponseEntity<List<TimeLog>> getAllTimeLogs() {
-        return ResponseEntity.ok(timeLogService.getAllTimeLogs());
+        LOGGER.info("Received request to get all time logs");
+        List<TimeLog> timeLogs = timeLogService.getAllTimeLogs();
+        LOGGER.info("Responding with {} time logs", timeLogs.size());
+        return ResponseEntity.ok(timeLogs);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TimeLog> getTimeLog(@PathVariable Long id) {
-        return ResponseEntity.ok(timeLogService.getTimeLog(id));
+        LOGGER.info("Received request to get time log with id {}", id);
+        TimeLog timeLog = timeLogService.getTimeLog(id);
+        LOGGER.info("Responding with time log with id {}", id);
+        return ResponseEntity.ok(timeLog);
     }
 
     @PostMapping
     public ResponseEntity<TimeLog> createTimeLog(@RequestBody TimeLog timeLog) {
-        return new ResponseEntity<>(timeLogService.saveTimeLog(timeLog), HttpStatus.CREATED);
+        LOGGER.info("Received request to create new time log");
+        TimeLog savedTimeLog = timeLogService.saveTimeLog(timeLog);
+        LOGGER.info("Created time log with id {}", savedTimeLog.getId());
+        return new ResponseEntity<>(savedTimeLog, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTimeLog(@PathVariable Long id) {
+        LOGGER.info("Received request to delete time log with id {}", id);
         timeLogService.deleteTimeLog(id);
+        LOGGER.info("Deleted time log with id {}", id);
         return ResponseEntity.noContent().build();
     }
 
@@ -73,6 +96,7 @@ public class TimeLogController {
                                                                       @RequestParam("end")
                                                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                                                       LocalDateTime end) {
+        LOGGER.info("Fetching time logs for user {} in date range from {} to {}", username, start, end);
         User user = userService.getUser(username);
         List<TimeLog> timeLogs = timeLogService.getTimeLogsByUserAndDateRange(user, start, end);
 
@@ -89,6 +113,7 @@ public class TimeLogController {
                 })
                 .collect(Collectors.toList());
 
+        LOGGER.info("Fetched {} time logs for user {} in date range from {} to {}", formattedTimeLogs.size(), username, start,end);
         return ResponseEntity.ok(formattedTimeLogs);
     }
 
@@ -101,7 +126,10 @@ public class TimeLogController {
                                                                          @RequestParam("end")
                                                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                                                          LocalDateTime end) {
+        LOGGER.info("Calculating total work effort for user {} in date range from {} to {}", username, start, end);
         User user = userService.getUser(username);
-        return ResponseEntity.ok(timeLogService.getTotalWorkEffortByUserAndDataRange(user, start, end));
+        Duration totalWorkEffort = timeLogService.getTotalWorkEffortByUserAndDataRange(user, start, end);
+        LOGGER.info("Total work effort for user {} id date range from {} to {} is {}", username, start, end, totalWorkEffort);
+        return ResponseEntity.ok(totalWorkEffort);
     }
 }
