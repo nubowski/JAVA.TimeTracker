d292605392ea700fba4e9c0a750d0864479d4c34
nubowski <nubowski@nnteam.ru>
2023-06-10 21:50:58 +0300

    feat: add handlers to GlobalExceptionHandler with HttpStatus

--- src/main/java/ru/nubowski/timeTracker/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/nubowski/timeTracker/exception/GlobalExceptionHandler.java
@@ -6,8 +6,50 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // found another way with typo, seems like the same result as is
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TimeLogNotFoundException.class)
+    public ResponseEntity<String> handleTimeLogNotFound(TimeLogNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(OngoingTaskNotFoundException.class)
+    public ResponseEntity<String> handleOngoingTaskNotFound(OngoingTaskNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CleanupFailedException.class)
+    public ResponseEntity<String> handleCleanupFailed(CleanupFailedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(TaskAlreadyExistsException.class)
+    public ResponseEntity<String> handleTaskAlreadyExists(TaskAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidDateFormatException.class)
+    public ResponseEntity<String> handleInvalidDateFormat(InvalidDateFormatException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoOngoingTaskException.class)
+    public ResponseEntity<String> handleNoOngoingTask(NoOngoingTaskException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
