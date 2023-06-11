fbeffc242a900fda8545571d2e64fa0f65e3c489
nubowski <nubowski@nnteam.ru>
2023-06-11 13:13:06 +0300

    feat: add test to UserService

--- src/test/java/ru/nubowski/timeTracker/service/UserServiceTest.java
+++ src/test/java/ru/nubowski/timeTracker/service/UserServiceTest.java
@@ -1,14 +1,18 @@
 package ru.nubowski.timeTracker.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.nubowski.timeTracker.exception.UserNotFoundException;
 import ru.nubowski.timeTracker.model.User;
 import ru.nubowski.timeTracker.repository.UserRepository;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,9 +28,18 @@ public class UserServiceTest {
 
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private TaskService taskService;
+    @MockBean
+    private TimeLogService timeLogService;
+
+    @BeforeEach
+    void setUp () {
+        userService = new UserService(userRepository, taskService, timeLogService);
+    }
 
     @Test
-    public void testCreateUser() {
+    void testCreateUser() {
         User user = new User();
         user.setEmail("test@test.com");
         user.setDisplayName("test");
@@ -39,7 +52,7 @@ public class UserServiceTest {
 
 
     @Test
-    public void testGetUserById() {
+    void testGetUserById() {
         User user = new User();
         user.setEmail("test@test.com");
         user.setUsername("test");
@@ -53,7 +66,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testUpdateUserEmail(){
+    void testUpdateUserEmail(){
         User user = new User();
         user.setEmail("test@test.com");
         user.setUsername("testUser");
@@ -67,7 +80,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testUpdateUsername(){
+    void testUpdateUsername(){
         User user = new User();
         user.setEmail("test@test.com");
         user.setUsername("testUser");
@@ -78,5 +91,47 @@ public class UserServiceTest {
         userService.saveUser(user);
 
         verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    void testGetUserByIdNotFound() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserById(1L);
+        });
+    }
+
+    //  TODO: add tasks check separately or here --V
+    @Test
+    void testDeleteUserAndTasks() {
+        User user = new User();
+        user.setUsername("username");
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        userService.deleteUserAndTasks("username");
+
+        verify(userRepository, times(1)).delete(user);
+        verify(taskService, times(user.getTasks().size())).deleteTask(anyLong());
+        verify(timeLogService, times(user.getTasks().size())).deleteTimeLogsForUser(user);
+    }
+
+    @Test
+    void testDeleteUserAndTasksUserNotFound() {
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.empty());
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.deleteUserAndTasks("test_username");
+        });
+    }
+
+    @Test // isolated context of cleanup service
+    void testDeleteOldUsers(){
+        LocalDateTime cutoff = LocalDateTime.now().minusDays(1);
+        User oldUser1 = new User();
+        oldUser1.setUsername("old1");
+        User oldUser2 = new User();
+        oldUser2.setUsername("old2");
+        when(userRepository.findByCreatedAtBefore(any(LocalDateTime.class))).thenReturn(Arrays.asList(oldUser1, oldUser2));
+        userService.deleteOldUsers(cutoff);
+        verify(userRepository, times(1)).deleteAll(Arrays.asList(oldUser1, oldUser2));
     }
 }
