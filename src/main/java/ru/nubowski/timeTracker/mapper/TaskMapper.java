f5d3cee477fc2e8a09a6a7d3897d1e2f5b306aad
nubowski <nubowski@nnteam.ru>
2023-06-16 04:35:36 +0300

    fix: Mapper SAVEs tasks

--- src/main/java/ru/nubowski/timeTracker/mapper/TaskMapper.java
+++ src/main/java/ru/nubowski/timeTracker/mapper/TaskMapper.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import ru.nubowski.timeTracker.dto.AllTasksGetResponse;
 import ru.nubowski.timeTracker.dto.TaskCreateRequest;
 import ru.nubowski.timeTracker.model.Task;
+import ru.nubowski.timeTracker.model.User;
+import ru.nubowski.timeTracker.service.impl.TaskService;
 import ru.nubowski.timeTracker.service.impl.UserService;
 
 @Service
@@ -13,18 +15,23 @@ public class TaskMapper {
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskMapper.class);
 
     private final UserService userService;
+    private final TaskService taskService;
 
-    public TaskMapper(UserService userService) {
+    public TaskMapper(UserService userService, TaskService taskService) {
         this.userService = userService;
+        this.taskService = taskService;
     }
 
 
     public Task mapToTask (TaskCreateRequest request, String username) {
         Task task = new Task();
         task.setUser(userService.getUserByUsernameNotOptional(username));
+        User user = userService.getUserByUsernameNotOptional(username);
+        LOGGER.info(user.toString());
         task.setName(request.getName());
         task.setDescription(request.getDescription());
-        // set other..
+
+        taskService.saveTask(task);
         return task;
     }
     public AllTasksGetResponse taskToAllTasksGetResponse(Task task) {
